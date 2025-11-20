3b21a501b1e870009d2ed5db71865e94974cb649
zhangqinglei <zhangqinglei@tal.com>
2025-11-20 18:18:55 +0800

    fix: remove deleted component imports from theme
    - Remove CounterDemo, ToggleDemo, LocalStorageDemo imports
    - Remove component registrations for deleted components
    - Fixes build error: Could not resolve CounterDemo.vue

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,5 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import CounterDemo from '../../components/CounterDemo.vue'
-import ToggleDemo from '../../components/ToggleDemo.vue'
-import LocalStorageDemo from '../../components/LocalStorageDemo.vue'
 import FetchDemo from '../../components/FetchDemo.vue'
 import AxiosFetchDemo from '../../components/AxiosFetchDemo.vue'
 import TableRequestDemo from '../../components/TableRequestDemo.vue'
@@ -13,9 +10,6 @@ export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // 注册全局组件
-    app.component('CounterDemo', CounterDemo)
-    app.component('ToggleDemo', ToggleDemo)
-    app.component('LocalStorageDemo', LocalStorageDemo)
     app.component('FetchDemo', FetchDemo)
     app.component('AxiosFetchDemo', AxiosFetchDemo)
     app.component('TableRequestDemo', TableRequestDemo)
